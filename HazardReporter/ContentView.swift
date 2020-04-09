086f1e1cd4a8062a7fe48eead70fbc5a90b731ce
Doug Robison <dfrobison@gmail.com>
2020-04-08 19:47:29 -0600

    Initial Commit

---
+++ HazardReporter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HazardReporter
+//
+//  Created by Doug Robison on 4/8/20.
+//  Copyright © 2020 Doug Robison. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
