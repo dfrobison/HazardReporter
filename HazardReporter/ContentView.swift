3b21e86931b8446ef5faecab7f665fd8ab39a478
Doug Robison <dfrobison@gmail.com>
2020-04-08 21:04:50 -0600

    Initial commit

--- HazardReporter/ContentView.swift
+++ HazardReporter/ContentView.swift
@@ -8,9 +8,61 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HazardItem: View {
+      var body: some View {
+        HStack {
+            Image
+            VStack {
+                Text
+                Text
+            }
+            
+        }
+    }
+}
+
+struct ActiveHazardsView: View {
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            VStack {
+                List {
+                    Text("HI")
+                }
+            }
+            .navigationBarTitle("Active Hazards", displayMode: .inline)
+            .navigationBarItems(trailing: Button(action: {})
+                {
+                    Image(systemName: "plus")
+                }
+            )
+        }
+    }
+}
+
+struct ResolvedHazardsView: View {
+    var body: some View {
+        NavigationView {
+            Text( "Resolved")
+        }.navigationBarTitle("Welcome")
+    }
+}
+
+struct ContentView: View {
+    @State var selectedView = 0
+
+    var body: some View {
+        TabView(selection: $selectedView) {
+            ActiveHazardsView()
+                .tabItem {
+                    Image("active-hazard-bar-button-icon")
+                    Text("Active Hazards")
+                }.tag(0)
+            ResolvedHazardsView()
+                .tabItem {
+                    Image("resolve-hazard-icon")
+                    Text("Resolved Hazards")
+                }.tag(1)
+        }
     }
 }
 
